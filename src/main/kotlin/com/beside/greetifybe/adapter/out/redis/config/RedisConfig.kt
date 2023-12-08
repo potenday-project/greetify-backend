cfe5740c910cb902263894bb5c7569a936bf179f
San Kim <waterfog9580@gmail.com>
2023-12-09 01:38:22 +0900

    ✨ Redis 설정 및 Handler 구현

---
+++ src/main/kotlin/com/beside/greetifybe/adapter/out/redis/config/RedisConfig.kt
@@ -0,0 +1,25 @@
+package com.beside.greetifybe.adapter.out.redis.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+
+@Configuration
+@EnableTransactionManagement
+class RedisConfig {
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.connectionFactory = redisConnectionFactory
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        redisTemplate.setEnableTransactionSupport(true)
+        return redisTemplate
+    }
+
+}
