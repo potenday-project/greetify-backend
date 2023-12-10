8553aa373ce0f6f5708d2071ff7eb4eb5a831cae
San Kim <waterfog9580@gmail.com>
2023-12-10 20:20:16 +0900

    🩹 Card id 타입 변경

--- src/main/kotlin/com/beside/greetifybe/domain/Card.kt
+++ src/main/kotlin/com/beside/greetifybe/domain/Card.kt
@@ -1,12 +1,12 @@
 package com.beside.greetifybe.domain
 
 import com.beside.greetifybe.domain.vo.IPAddress
-import com.github.f4b6a3.ulid.Ulid
 import com.github.f4b6a3.ulid.UlidCreator
 import java.time.LocalDateTime
+import java.util.*
 
 data class Card(
-    val id: Ulid = UlidCreator.getMonotonicUlid(),
+    val id: UUID = UlidCreator.getMonotonicUlid().toUuid(),
     val userIp: IPAddress,
     val cardDesignId: Long,
     val phrase: String,
