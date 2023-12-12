16e4966e0dcd5720078223771f5d2de8b84d7e99
San Kim <waterfog9580@gmail.com>
2023-12-12 20:39:17 +0900

    ♻️ CLOVA 튜닝 적용

--- src/main/kotlin/com/beside/greetifybe/adapter/out/http/ncp/clova/dto/NcpClovaCardPhraseGenerator.kt
+++ src/main/kotlin/com/beside/greetifybe/adapter/out/http/ncp/clova/dto/NcpClovaCardPhraseGenerator.kt
@@ -1,17 +1,69 @@
 package com.beside.greetifybe.adapter.out.http.ncp.clova.dto
 
+import com.beside.greetifybe.domain.enums.Age
+import com.beside.greetifybe.domain.enums.Dialect
+import com.beside.greetifybe.domain.enums.Emotional
+import com.beside.greetifybe.domain.enums.Season
+
 data class NcpClovaCardPhraseGeneratorRequest(
-    val text: String,
-    val start: String = "",
-    val restart: String = "",
-    val includeTokens: Boolean = false,
+    val messages: List<Message>,
     val topP: Double = 0.8,
     val topK: Int = 0,
     val maxTokens: Int = 60,
     val temperature: Double = 0.5,
     val repeatPenalty: Double = 5.0,
-    val stopBefore: List<String> = listOf("###"),
+    val stopBefore: List<String> = listOf(),
     val includeAiFilters: Boolean = true
+) {
+
+    companion object {
+        fun of(
+            season: Season,
+            emotional: List<Emotional>,
+            age: List<Age>,
+            dialect: Dialect,
+            words: List<String>,
+        ): NcpClovaCardPhraseGeneratorRequest {
+            val trainPrompt: String =
+                """
+                시즌과 감정, 화자의 연령대, 말투, 단어를 입력으로 전달하면 이를 반영한 카드 문구를 생성한다.
+                (최소 10자, 최대 60자 이내)
+                입력 예시)
+                시즌 : 신년 감정 : 공부 연령대 : 40대 말투 : 충청도 사투리 단어 : 고생했어, 즐거운
+                출력 예시)
+                신년이 밝았슈! 작년 한 해 고생 많았쥬? 올해는 즐거운 일만 가득하길 바라유. 
+                같이 공부 열심히 혀서 좋은 결과 얻어 봅시다.
+            """.trimIndent()
+
+            val trainMessage = Message(
+                role = "system",
+                content = trainPrompt
+            )
+
+            val targetContent = """
+                시즌 : ${season.value}
+                감정 : ${emotional.joinToString(", ") { it.value }} }}
+                연령대 : ${age.joinToString(", ") { it.value }}
+                말투 : ${dialect.value}
+                단어 : ${words.joinToString(", ")}
+            """.trimIndent()
+
+            val targetMessage = Message(
+                role = "user",
+                content = targetContent
+            )
+
+            return NcpClovaCardPhraseGeneratorRequest(
+                messages = listOf(trainMessage, targetMessage)
+            )
+        }
+    }
+
+}
+
+data class Message(
+    val role: String,
+    val content: String
 )
 
 data class NcpClovaCardPhraseGeneratorResponse(
@@ -25,16 +77,10 @@ data class Status(
 )
 
 data class Result(
-    val text: String,
-    val stopReason: String,
-    val inputText: String,
+    val message: Message,
     val inputLength: Int,
-    val inputTokens: List<String>,
-    val outputText: String,
     val outputLength: Int,
-    val outputTokens: List<String>,
-    val probs: List<Double>,
-    val ok: Boolean,
+    val stopReason: String,
     val aiFilter: List<AiFilter>
 )
 
