4fa3fd7fe6aa52ee5697d6855f779808b89397a7
San Kim <waterfog9580@gmail.com>
2023-12-12 22:05:59 +0900

    ✨ Feign 재시도 설정

--- src/main/kotlin/com/beside/greetifybe/adapter/out/http/config/FeignConfig.kt
+++ src/main/kotlin/com/beside/greetifybe/adapter/out/http/config/FeignConfig.kt
@@ -1,10 +1,20 @@
 package com.beside.greetifybe.adapter.out.http.config
 
+import feign.Retryer
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.TimeUnit
 
 @EnableFeignClients(
     basePackages = [
         "com.beside.greetifybe.adapter.out.http",
     ],
 )
-class FeignConfig
+class FeignConfig {
+
+    @Bean
+    fun retryer(): Retryer.Default {
+        return Retryer.Default(100L, TimeUnit.SECONDS.toMillis(3L), 5);
+    }
+
+}
