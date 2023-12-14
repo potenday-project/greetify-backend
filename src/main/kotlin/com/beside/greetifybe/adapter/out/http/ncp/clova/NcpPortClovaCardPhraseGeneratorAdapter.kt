381c5f15414ceb4229e6fb3ec2b5dee6b6ac47ca
San Kim <waterfog9580@gmail.com>
2023-12-14 16:48:06 +0900

    ✨ Clova 사전 프롬프트 수정

--- src/main/kotlin/com/beside/greetifybe/adapter/out/http/ncp/clova/NcpPortClovaCardPhraseGeneratorAdapter.kt
+++ src/main/kotlin/com/beside/greetifybe/adapter/out/http/ncp/clova/NcpPortClovaCardPhraseGeneratorAdapter.kt
@@ -3,17 +3,27 @@ package com.beside.greetifybe.adapter.out.http.ncp.clova
 import com.beside.greetifybe.adapter.out.http.ncp.clova.dto.NcpClovaCardPhraseGeneratorRequest
 import com.beside.greetifybe.adapter.out.http.ncp.clova.dto.NcpClovaCardPhraseGeneratorResponse
 import com.beside.greetifybe.application.port.out.CardPhraseGeneratorPort
+import com.beside.greetifybe.common.exception.ApiExceptionType
+import com.beside.greetifybe.common.exception.CustomException
 import com.beside.greetifybe.domain.enums.Age
 import com.beside.greetifybe.domain.enums.Dialect
 import com.beside.greetifybe.domain.enums.Emotional
 import com.beside.greetifybe.domain.enums.Season
 import org.springframework.stereotype.Component
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @Component
 class NcpPortClovaCardPhraseGeneratorAdapter(
     private val properties: NcpClovaProperties,
     private val api: NcpClovaStudioApi,
 ) : CardPhraseGeneratorPort {
+
+    companion object {
+        private const val PRE_TRAINED_PROMPT_FILE = "PreTrainPrompt.txt"
+    }
+
 
     override fun generate(
         season: Season,
@@ -22,7 +32,9 @@ class NcpPortClovaCardPhraseGeneratorAdapter(
         dialect: Dialect,
         word: List<String>
     ): String {
+        val preTrainPrompt: String = readPreTrainPromptFromFile()
         val request: NcpClovaCardPhraseGeneratorRequest = NcpClovaCardPhraseGeneratorRequest.of(
+            preTrainPrompt = preTrainPrompt,
             season = season,
             emotional = emotional,
             age = age,
@@ -38,6 +50,21 @@ class NcpPortClovaCardPhraseGeneratorAdapter(
                 request = request
             )
 
-        return response.result.message.content
+        val content: String = response.result.message.content
+        return removeQuotationAndNewLine(content)
+    }
+
+    private fun removeQuotationAndNewLine(content: String): String {
+        return content
+            .replace("\"", "")
+            .replace("\n", "")
+    }
+
+    private fun readPreTrainPromptFromFile(): String {
+        val uri: URI = javaClass.classLoader.getResource(PRE_TRAINED_PROMPT_FILE)?.toURI() ?: throw CustomException(
+            ApiExceptionType.RESOURCE_NOT_FOUND,
+            "PreTrainPrompt.txt 파일을 찾을 수 없습니다."
+        )
+        return String(Files.readAllBytes(Paths.get(uri)))
     }
 }
--- src/main/kotlin/com/beside/greetifybe/adapter/out/http/ncp/clova/dto/NcpClovaCardPhraseGenerator.kt
+++ src/main/kotlin/com/beside/greetifybe/adapter/out/http/ncp/clova/dto/NcpClovaCardPhraseGenerator.kt
@@ -9,7 +9,7 @@ data class NcpClovaCardPhraseGeneratorRequest(
     val messages: List<Message>,
     val topP: Double = 0.8,
     val topK: Int = 0,
-    val maxTokens: Int = 60,
+    val maxTokens: Int = 40,
     val temperature: Double = 0.5,
     val repeatPenalty: Double = 5.0,
     val stopBefore: List<String> = listOf(),
@@ -18,35 +18,25 @@ data class NcpClovaCardPhraseGeneratorRequest(
 
     companion object {
         fun of(
+            preTrainPrompt: String,
             season: Season,
             emotional: List<Emotional>,
             age: List<Age>,
             dialect: Dialect,
             words: List<String>,
         ): NcpClovaCardPhraseGeneratorRequest {
-            val trainPrompt: String =
-                """
-                시즌과 감정, 화자의 연령대, 말투, 단어를 입력으로 전달하면 이를 반영한 카드 문구를 생성한다.
-                (최소 10자, 최대 60자 이내)
-                입력 예시)
-                시즌 : 신년 감정 : 공부 연령대 : 40대 말투 : 충청도 사투리 단어 : 고생했어, 즐거운
-                출력 예시)
-                신년이 밝았슈! 작년 한 해 고생 많았쥬? 올해는 즐거운 일만 가득하길 바라유. 
-                같이 공부 열심히 혀서 좋은 결과 얻어 봅시다.
-            """.trimIndent()
-
             val trainMessage = Message(
                 role = "system",
-                content = trainPrompt
+                content = preTrainPrompt,
             )
 
-            val targetContent = """
-                시즌 : ${season.value}
-                감정 : ${emotional.joinToString(", ") { it.value }} }}
-                연령대 : ${age.joinToString(", ") { it.value }}
-                말투 : ${dialect.value}
-                단어 : ${words.joinToString(", ")}
-            """.trimIndent()
+            val targetContent = TargetPrompt(
+                season = season,
+                emotional = emotional,
+                age = age,
+                dialect = dialect,
+                words = words
+            ).toDialogTurn()
 
             val targetMessage = Message(
                 role = "user",
@@ -59,6 +49,24 @@ data class NcpClovaCardPhraseGeneratorRequest(
         }
     }
 
+}
+
+data class TargetPrompt(
+    val season: Season,
+    val emotional: List<Emotional>,
+    val age: List<Age>,
+    val dialect: Dialect,
+    val words: List<String>,
+) {
+    fun toDialogTurn(): String {
+        return """
+            시즌 : ${season.value}
+            감정 : ${emotional.joinToString(", ") { it.value }} }}
+            연령대 : ${age.joinToString(", ") { it.value }}
+            말투 : ${dialect.value}
+            단어 : ${words.joinToString(", ")}
+        """.trimIndent()
+    }
 }
 
 data class Message(
